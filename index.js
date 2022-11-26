4ff73c6174f02bf79d4663e594dc3a97d2fad896
Marco Franca <dev.marcotullio@gmail.com>
2022-11-26 18:07:44 -0300

    database connection [update]

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+//bd
 const connection = require('./database/database')
+// const Category = require('./categories/Category')
+// const Article = require('./articles/Article')
+
+//controllers
+const categoriesController = require("./categories/categoriesController")
+const articlesController = require('./articles/articlesController')
 
 //view engine
 app.set('view engine','ejs');
 
 //static
-app.set(express.static('public'));
+app.use(express.static('public'));
 
 //body parser
 app.use(bodyParser.urlencoded({extended:false}));
@@ -19,10 +26,17 @@ connection.authenticate().then(()=>{
     console.log("conexao com sucesso")
 }).catch((err)=> console.log(err))
 
+//controllers
+
+app.use('/',categoriesController)
+app.use('/',articlesController)
+
 app.get('/',(req,res)=>{
     res.render('index');
 })
 
-app.listen(8080,()=>{
-    console.log("o servidor está rodando!")
+
+
+app.listen(8000,()=>{
+    console.log("o servidor está rodando na porta: 8000")
 })
