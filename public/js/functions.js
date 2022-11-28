1c61759f7f93449151ad8f0c89a61f81d04c91b0
Marco Franca <dev.marcotullio@gmail.com>
2022-11-28 11:51:22 -0300

    tinimce install [update]

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,7 +1,12 @@
-    function deleteConfirm (event, form){
+function deleteConfirm (event, form){
     event.preventDefault()
     let decision = confirm("Você realmente deseja deletar permanentemente esta categoria?")
     if (decision){
     form.submit()
 }
+
+tinymce.init({
+    selector:"#article"
+})
+
 }
