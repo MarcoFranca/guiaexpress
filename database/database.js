e854a83c4b23c681dc17a368076520da1e84271b
Marco Franca <dev.marcotullio@gmail.com>
2022-11-30 19:44:00 -0300

    time zone adjustment [update]

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize")
 
 const connection = new Sequelize('guiapress','root', 'M@t27111829',{
     host:"localhost",
-    dialect:"mysql"
+    dialect:"mysql",
+    timezone:"-03:00"
 });
 
 module.exports = connection;
