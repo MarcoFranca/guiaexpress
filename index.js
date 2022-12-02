ef430665570c3fd6b88e867abea0912d36d89b9c
Marco Franca <dev.marcotullio@gmail.com>
2022-12-02 07:33:19 -0300

    table implementation and new controllers [update]

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const session = require('express-session');
 //bd
-const connection = require('./database/database')
-const Category = require('./categories/Category')
-const Article = require('./articles/Article')
+const connection = require('./database/database');
+const Category = require('./categories/Category');
+const Article = require('./articles/Article');
+const User = require('./users/User');
 
 //controllers
-const categoriesController = require("./categories/categoriesController")
-const articlesController = require('./articles/articlesController')
+const categoriesController = require("./categories/categoriesController");
+const articlesController = require('./articles/articlesController');
+const usersController = require('./users/usersController');
+const {router} = require("express/lib/application");
 
 //view engine
 app.set('view engine','ejs');
+
+//session
+
+app.use(session({
+    secret:"cachorromolhado",
+    cookie:{ maxAge: 60 * 60000}
+}))
 
 //static
 app.use(express.static('public'));
@@ -23,20 +34,59 @@ app.use(bodyParser.json());
 //database
 
 connection.authenticate().then(()=>{
-    console.log("conexao com sucesso")
-}).catch((err)=> console.log(err))
+    console.log("conexao com sucesso");
+}).catch((err)=> console.log(err));
 
 //controllers
 
-app.use('/',categoriesController)
-app.use('/',articlesController)
+app.use('/',categoriesController);
+app.use('/',articlesController);
+app.use('/',usersController);
 
-app.get('/',(req,res)=>{
-    res.render('index');
+
+
+app.get('/:slug',(req, res)=>{
+    const slug = req.params.slug;
+    Article.findOne({
+        where:{
+            slug:slug
+        }
+    }).then((article)=>{
+        if(article){
+            Category.findAll().then(categories=>{
+            res.render('article',{ article: article, categories:categories });
+            })
+        }else {
+            res.redirect('/');
+        }
+    }).catch(err=> {
+        console.log(err)
+        res.redirect('/');
+    })
+})
+
+app.get('/category/:slug',(req, res)=>{
+    const slug = req.params.slug;
+    Category.findOne({where:{
+        slug:slug
+        },
+    include:[{model: Article}]
+    }).then(category =>{
+        if (category){
+            Category.findAll().then(categories=>{
+                res.render('index',{articles: category.articles, categories:categories});
+            });
+        }else {
+            res.redirect('/');
+        }
+    }).catch(err=>{
+        console.log(err)
+        res.redirect("/");
+    })
 })
 
 
 
 app.listen(8000,()=>{
-    console.log("o servidor está rodando na porta: 8000")
+    console.log("o servidor está rodando na porta: 8000");
 })
